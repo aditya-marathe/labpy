bbcdab749c0eb45072d8e58ad86a514273bb8eb5
aditya-marathe <72881014+aditya-marathe@users.noreply.github.com>
2022-08-19 17:14:30 +0100

    Version 1.1 update (check description)
    Compatible with Python 3.9 or lower, improved validation and other minor changes.

--- labpy/propagate.py
+++ labpy/propagate.py
@@ -1,12 +1,18 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-__all__ = ('propagate_errors',)
-__version__ = '1.0'
+__all__ = ('propagate_errors',
+           'partial_derivative',
+           'DELTA',
+           'ARRAY_SIZE',
+           'InvalidPropagateFunctionError')
+__version__ = '1.1'
 __author__ = 'Aditya Marathe'
 
 from functools import wraps
 from typing import Callable
+from typing import Union
+from typing import Dict
 
 import numpy as np
 
@@ -16,10 +22,14 @@ DELTA = 0.1
 ARRAY_SIZE = 100
 
 
+class InvalidPropagateFunctionError(Exception):
+    pass
+
+
 def _pd_helper_func(
-        f: callable,
+        f: Callable,
         **parameters
-) -> dict[str, float]:
+) -> Dict[str, float]:
     output = dict()
     for parameter, value in parameters.items():
         o_parameters = parameters.copy()
@@ -33,7 +43,7 @@ def _pd_helper_func(
 def partial_derivative(
         f: Callable,
         **parameters
-) -> dict[str, float | list]:
+) -> Dict[str, Union[float, list]]:
     _n_parameters = len(parameters)
     _parameter_names = list(parameters.keys())
     _parameter_values = list(parameters.values())
@@ -59,7 +69,7 @@ def partial_derivative(
 
         return result
 
-    elif isinstance(_parameter_values[0], int | float):
+    elif isinstance(_parameter_values[0], Union[int, float]):
         # Calculate the partial derivative: if number
         return _pd_helper_func(f, **parameters)
 
@@ -68,20 +78,43 @@ def partial_derivative(
 
 def propagate_errors(f: Callable) -> Callable:
     @wraps(f)
-    def wrapper(**kwargs) -> int | float | np.ndarray | Quantity:
-        _kwarg_values = list(kwargs.values())
+    def wrapper(*a, **kw) -> Union[int, float, np.ndarray, Quantity]:
+        if len(a):
+            raise RuntimeError('Function only allows use of keyword arguments.')
+
+        _kwarg_values = list(kw.values())
 
         if all(isinstance(val, Quantity) for val in _kwarg_values):
             quantity_values = dict()
             quantity_errors = dict()
 
-            for q_name, q in list(kwargs.items()):
+            for q_name, q in list(kw.items()):
                 quantity_values[q_name] = q.val
                 quantity_errors[q_name] = q.err
 
+            input_values = list(quantity_values.values())[0]
+            output_values = f(**quantity_values)
+
+            # Check for a reasonable output:
+            # - f(num) -> num
+            # - f(arr) -> arr
+
+            if not isinstance(output_values, type(input_values)):
+                raise InvalidPropagateFunctionError('Output type must match input type.')
+
+            if isinstance(output_values, np.ndarray):
+                input_shape = output_values.shape
+                output_shape = input_values.shape
+                if output_shape != input_shape:
+                    raise InvalidPropagateFunctionError('Input and output array shapes do not match.'
+                                                        f'Got: `input.shape = {input_shape}` and '
+                                                        f'`output.shape = {output_shape}`.')
+
+            # Calculations
             df_d = partial_derivative(f, **quantity_values)
             df_d_values = list(df_d.values())
-            if isinstance(df_d_values[0], list | np.ndarray):
+
+            if isinstance(df_d_values[0], Union[list, np.ndarray]):
                 list_of_squares = np.zeros(len(df_d_values[0]))
             else:
                 list_of_squares = 0
@@ -89,9 +122,9 @@ def propagate_errors(f: Callable) -> Callable:
             for var_name, var_value in list(df_d.items()):
                 list_of_squares += (var_value * quantity_errors[var_name]) ** 2
 
-            return Quantity(f(**quantity_values), np.sqrt(list_of_squares))
+            return Quantity(output_values, np.sqrt(list_of_squares))
 
-        elif all(isinstance(val, int | float | np.ndarray) for val in _kwarg_values):
-            return f(**kwargs)
+        elif all(isinstance(val, Union[int, float, np.ndarray]) for val in _kwarg_values):
+            return f(**kw)
 
     return wrapper
